f2f8b9faa3502460eef1188a36a1922c46364262
Vinicius Martins <vinima@microsoft.com>
2020-11-05 18:37:06 -0700

    add function to copy to another blog storage

--- WAF Logs/WafLogsCopier.cs
+++ WAF Logs/WafLogsCopier.cs
@@ -6,16 +6,19 @@ using Microsoft.Extensions.Logging;
 using Microsoft.Azure.WebJobs.Extensions.DataLake;
 using System.IO.Compression;
 using System.Threading.Tasks;
+using Microsoft.Azure.Storage.Blob;
+using Microsoft.WindowsAzure.Storage.Blob;
 
 namespace WAF_Logs
 {
     public static class WafLogsCopier
     {
-        [FunctionName(nameof(CopyWafLogs))]
-        public static async Task CopyWafLogs(
-            [BlobTrigger("insights-logs-webapplicationfirewalllogs/{name}", Connection = "WAFLogsStorage")]
-            Stream myBlob, 
-            string name,
+        [FunctionName(nameof(CopyToDataLake))]
+        public static async Task CopyToDataLake(
+            [BlobTrigger("insights-logs-webapplicationfirewalllogs/{blobName}.{blobExtension}", Connection = "WAFLogsStorage")]
+            Stream inboundBlob,
+            string blobName,
+            string blobExtension,
             [DataLakeStore(
               AccountFQDN = "%fqdn%",
               ApplicationId = "%applicationid%",
@@ -24,65 +27,89 @@ namespace WAF_Logs
             IAsyncCollector<DataLakeStoreOutput> asyncCollector,
             ILogger log)
         {
-            var fileStream = myBlob;
+            var fileStream = inboundBlob;
 
-            if (IsCompressedFile(name))
+            if (IsCompressedFile(blobExtension))
             {
                 try
                 {
-                    var decompressed = await Decompress(name, myBlob, log);
-
-                    name = decompressed.fileName;
-                    fileStream = decompressed.fileStream;
+                    var decompressed = await Decompress(inboundBlob);
+                    decompressed.Seek(0, SeekOrigin.Begin);
+                    
+                    fileStream = decompressed;
                 }
                 catch(Exception ex)
                 {
-                    log.LogError("Failed to decompress blob:\n Name:{name}\n Reason:{reason}", name, ex.Message);
+                    log.LogError("Failed to decompress blob:\n Name:{name}\n Reason:{reason}", blobName, ex.Message);
                     throw;
                 }
-                
             }
 
             var dataLakeStoreOutput = new DataLakeStoreOutput()
             {
-                FileName = "/mydata/" + name,
+                FileName = "/mydata/" + $"{blobName}.json",
                 FileStream = fileStream
             };
 
             await asyncCollector.AddAsync(dataLakeStoreOutput);
 
-            log.LogInformation($"Processed blob\n Name:{name} \n Size: {fileStream.Length} Bytes");
+            log.LogInformation($"Processed blob\n Name:{blobName} \n Size: {fileStream.Length} Bytes");
         }
 
-        private static bool IsCompressedFile(string fileName)
+        [FunctionName(nameof(CopyToStorage))]
+        public static async Task CopyToStorage(
+            [BlobTrigger("insights-logs-webapplicationfirewalllogs/{blobName}.{blobExtension}", Connection = "WAFLogsStorage")]
+            Stream inboundBlob,
+            string blobName,
+            string blobExtension,
+            [Blob("insights-logs-webapplicationfirewalllogs-uncompressed/{blobName}.json", FileAccess.Write, Connection = "WAFLogsStorage")]
+            Stream outboundBlob,
+            ILogger log)
         {
-            if (string.IsNullOrEmpty(fileName))
+            var fileStream = inboundBlob;
+
+            if (IsCompressedFile(blobExtension))
+            {
+                try
+                {
+                    var decompressed = await Decompress(inboundBlob, outboundBlob);
+                }
+                catch (Exception ex)
+                {
+                    log.LogError("Failed to decompress blob:\n Name:{name}\n Reason:{reason}", blobName, ex.Message);
+                    throw;
+                }
+            }
+            else
+            {
+                await inboundBlob.CopyToAsync(outboundBlob);
+            }
+
+            log.LogInformation($"Processed blob\n Name:{blobName} \n Size: {fileStream.Length} Bytes");
+        }
+
+        private static bool IsCompressedFile(string ext)
+        {
+            if (string.IsNullOrEmpty(ext))
             {
                 return false;
             }
 
-            return fileName.EndsWith(".gz");
+            return ext.Equals("gz", StringComparison.OrdinalIgnoreCase);
         }
 
-        private static async Task<(string fileName, Stream fileStream)> Decompress(string name, Stream compressedFile, ILogger log)
+        private static async Task<Stream> Decompress(Stream compressedFile, Stream decompressedFile = null)
         {
-            if (!IsCompressedFile(name))
+            if(decompressedFile == null)
             {
-                return (null, null);
+                decompressedFile = new MemoryStream();
             }
 
-            MemoryStream decompressedFile = new MemoryStream();
             using GZipStream gZipStream = new GZipStream(compressedFile, CompressionMode.Decompress, true);
 
-            log.LogDebug("Decompressing blob:\n Name:{name} ", name);
             await gZipStream.CopyToAsync(decompressedFile);
-            decompressedFile.Seek(0, SeekOrigin.Begin);
-
-            log.LogDebug("Changing file extension", name);
-            string newName = ChangeFileExtension(name);
-            log.LogDebug("New file name:\n {name}", newName);
-
-            return (newName, decompressedFile);
+            
+            return decompressedFile;
         }
 
         private static string ChangeFileExtension(string name)
